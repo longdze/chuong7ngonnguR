6578b22df3b9eb98ac31d9f59ba3d6b710a559a4
longdze <115168657+longdze@users.noreply.github.com>
2023-04-06 22:42:45 +0700

    Delete 7.6 The Pairplot.R

--- 7.6 The Pairplot.R
+++
@@ -1,7 +0,0 @@
-setwd("C:/Users/ACER/Downloads/Dataset/Dataset")
-Benthic <- read.table(file = "RIKZ2.txt", header = TRUE )
-pairs(Benthic[, 2:9])
-#
-#7.6.1 Panel Functions
-pairs(Benthic[, 2:9], diag.panel = panel.hist, upper.panel = panel.smooth, lower.panel = panel.cor)
-# 
